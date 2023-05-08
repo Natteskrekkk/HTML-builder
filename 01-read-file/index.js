d9ba3739b0fc1e080514033e176e24a22808cc7f
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-05-09 00:55:57 +0300

    feat: add solution of task 2

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,6 +6,10 @@ const fs = require("fs");
 //         console.log(data);
 //     });
 
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+emitter.on('start', () => console.log('Привет, сейчас ты проверяешь задание 1. Текст выведен ниже:'));
+emitter.emit('start');
 
 const readableStream = fs.createReadStream("./01-read-file/text.txt", "utf-8");
 readableStream.on('error', function (error) {
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,5 +1,32 @@
 const fs = require("fs");
+const path = require('path');
+const { stdin, stdout, stderr } = process;
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+emitter.on('start', () => console.log('Привет, сейчас ты проверяешь задание 2. Введи текст ниже:'));
+emitter.emit('start');
 
-fs.writeFile("hello.txt", "Hello мир!", function (error) {
-    if (error) throw error; // если возникла ошибка
+fs.writeFile(path.join(__dirname, "write.txt"), "", function (err, data) {
+    if (err) throw err;
+})
+
+stdin.on('data', (data) => {
+    fs.appendFile(path.join(__dirname, "write.txt"), data, function (err, data) {
+        if (err) throw err;
+    })
+    if (data.toString().trim() == "exit") {
+        process.exit();
+    }
+});
+
+process.on('SIGINT', () => {
+    process.exit();
+});
+
+process.on('exit', code => {
+    if (code === 0) {
+        stdout.write("\nУдачи в изучении Node.js!");
+    } else {
+        stderr.write(`Что-то пошло не так. Программа завершилась с кодом ${code}`);
+    }
 });

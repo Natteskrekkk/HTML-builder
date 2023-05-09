82e7688e9f302feed11366ecbf7b618d265c107c
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-05-09 23:04:46 +0300

    feat: add solution of task 3

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,11 @@
-const fs = require("fs");
-
-// fs.readFile("./01-read-file/text.txt", "utf8",
-//     function (error, data) {
-//         if (error) throw error;
-//         console.log(data);
-//     });
-
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 emitter.on('start', () => console.log('Привет, сейчас ты проверяешь задание 1. Текст выведен ниже:'));
 emitter.emit('start');
+
+
+
+const fs = require("fs");
 
 const readableStream = fs.createReadStream("./01-read-file/text.txt", "utf-8");
 readableStream.on('error', function (error) {
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,28 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
-emitter.on('start', () => console.log('Привет, на больших выходных помогал родителям на даче, поэтому не успел \nвыполнить все пункты. Буду рад, если отложишь проверку на последний \nдень или оставишь свой дискорд открытым (спамить не буду, просто \nнапишу один раз когда доделаю и уже на свое усмотрение \nрешишь перепроверить работу или нет).'));
+emitter.on('start', () => console.log('Привет, сейчас ты проверяешь задание 3:'));
 emitter.emit('start');
+
+
+const fs = require('fs');
+const path = require('path');
+const directoryPath = path.join(__dirname, "secret-folder");
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function (file) {
+
+        fs.stat(path.join(directoryPath, file), function (err, stats) {
+            if (err) {
+                return console.log('Unable to get stat: ' + err);
+            }
+            if (stats.isFile()) {
+                console.log(path.parse(file).name + " - " + path.parse(file).ext.substring(1, undefined) + " - " + stats.size * 0.001 + "kb")
+            }
+        })
+
+
+    });
+});
